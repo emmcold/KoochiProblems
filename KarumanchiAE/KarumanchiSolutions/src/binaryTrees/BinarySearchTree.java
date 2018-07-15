4d6537e4167f953f458dc03704ce7088d73a9e13
Emm <ez19571@seeu.edu.mk>
2018-07-15 13:06:43 +0200

    Adding changes to BST

--- KarumanchiAE/KarumanchiSolutions/src/binaryTrees/BinarySearchTree.java
+++ KarumanchiAE/KarumanchiSolutions/src/binaryTrees/BinarySearchTree.java
@@ -24,18 +24,18 @@ public class BinarySearchTree {
      */
     public void createTree(){
         root=new Node(62);
-        root.left=new Node(4);
-        root.right=new Node(49);
-        root.left.left=new Node(69);
-        root.left.right=new Node(-109);
+        root.left=new Node(50);
+        root.right=new Node(69);
+        root.left.left=new Node(43);
+        root.left.right=new Node(52);
         root.left.left.left=new Node(-102);
-        root.left.left.right=new Node(-40);
-        root.right.left=new Node(22);
-        root.right.left.right = new Node(-38);
-        root.right.right=new Node(0);
-        root.right.right.left=new Node(629);
-        root.right.right.left.left=new Node(44);
-        root.right.right.left.right=new Node(100);
+        root.left.left.right=new Node(47);
+        root.right.left=new Node(67);
+        root.right.left.right = new Node(68);
+        root.right.right=new Node(78);
+        root.right.right.left=new Node(72);
+        root.right.right.left.left=new Node(62);
+        root.right.right.left.right=new Node(75);
     }
     
     /**
--- KarumanchiAE/KarumanchiSolutions/src/binaryTrees/LauncherBST.java
+++ KarumanchiAE/KarumanchiSolutions/src/binaryTrees/LauncherBST.java
@@ -9,24 +9,43 @@ public class LauncherBST {
         bst.createTree();
         bst.levelOrderTraversal(bst.root);
         
-        Node findEl = findElement(bst.root, 62);
+        Node findEl = findElement(bst.root, -33102);
         if(findEl!=null){
         System.out.println("\nThe element is: "+findEl.data);}
         else{
             System.out.println("\nThe element is not found!");
         }
+        int i = 0;
+        int min=findMinimumElement(bst.root);
+        System.out.println("\nThe min is: "+min);
         
     }
     
     public static Node findElement(Node root, int el){
-        if(root == null) return root;
+        if(root == null) return null;
         
         if(root.data == el) return root;
-        Node leftSubTree = findElement(root.left, el);
-        Node rightSubTree = findElement(root.right, el);
         
-        if(leftSubTree != null) return leftSubTree;
-        if(rightSubTree != null) return rightSubTree;
-        return null;
+        if(root.data < el){
+           // Node rightSubTree = findElement(root.right, el);
+            return findElement(root.right, el);
+        }
+        else if(root.data > el){
+            //Node leftSubTree = findElement(root.left, el);
+            return findElement(root.left, el);
+        }
+       
+        return root;
     }
+    
+    public static int findMinimumElement(Node root){
+        if(root==null) return Integer.MAX_VALUE;
+        
+        Node traverse = root;
+        while(traverse.left !=null){
+            traverse = traverse.left;
+        }
+        return traverse.data;
+                
+        }
 }
