9cfa75dd3d299f4272ef325f6a19b8357643802f
EmmCold <ez19571@seeu.edu.mk>
2020-01-07 22:03:22 +0100

    GetMin element in stack in constant time

--- KarumanchiAE/KarumanchiSolutions/src/stack/StackProblems.java
+++ KarumanchiAE/KarumanchiSolutions/src/stack/StackProblems.java
@@ -44,11 +44,37 @@ public class StackProblems {
 		}
 		return false;
 	}
+	/**
+	 * Design a stack such that the getMinimum is O(1)
+	 * @param args
+	 */
+	private static int getMinimum(Stack<Integer> mainStack) {
+		Stack<Integer> minStack = new Stack<>();
+		minStack.push(mainStack.pop());
+		
+		while(!mainStack.isEmpty()) {
+			if(minStack.peek()> mainStack.peek()) {
+					minStack.push(mainStack.pop());
+			}
+			else {
+				mainStack.pop();
+			}
+		}
+		return minStack.pop();
+	}
 	
 	public static void main(String[] args){
 		String string = "(A+B)-[C-D]";
 		System.out.println("The string "+string+ "has balanced paranthesis: "
 		+problem1(string));
 		
+		Stack<Integer> stack = new Stack<>();
+		stack.add(2);
+		stack.add(3);
+		stack.add(1);
+		stack.add(15);
+		stack.add(16);
+		System.out.println("Finding the min: "+ getMinimum(stack));
+	
 	}
 }
